cc390a62b1dc5667f6bd827adb61612c1261af97
Baocute123 <10423009@student.vgu.edu.vn>
2025-06-04 21:10:33 +0700

    Matrix.cpp

--- Matrix.cpp
+++ Matrix.cpp
@@ -26,19 +26,20 @@ private:
     }
 
 public:
+//Constructor
     Matrix(int rows, int cols) : mNumRows(rows), mNumCols(cols) {
         if (rows <= 0 || cols <= 0)
             throw invalid_argument("Dimension must be positive");
         allocateMemory();
     }
-
+//Copy constructor
     Matrix(const Matrix& other) : mNumRows(other.mNumRows), mNumCols(other.mNumCols) {
         allocateMemory();
         for (int i = 0; i < mNumRows; ++i)
             for (int j = 0; j < mNumCols; ++j)
                 mData[i][j] = other.mData[i][j];
     }
-
+//Copy assignment operator
     Matrix& operator=(const Matrix& other) {
         if (this == &other) return *this;
         freeMemory();
@@ -50,14 +51,14 @@ public:
                 mData[i][j] = other.mData[i][j];
         return *this;
     }
-
+//Destructor
     ~Matrix() {
         freeMemory();
     }
-
+//Access row numbers and col numbers
     int Rows() const { return mNumRows; }
     int Cols() const { return mNumCols; }
-
+//round bracket operator with one0based index
     double& operator()(int i, int j) {
         if (i < 0 || i >= mNumRows || j < 0 || j >= mNumCols)
             throw out_of_range("Out of bounds");
@@ -69,7 +70,7 @@ public:
             throw out_of_range("Out of bounds");
         return mData[i][j];
     }
-
+//Addition
     Matrix operator+(const Matrix& other) const {
         assert(mNumRows == other.mNumRows && mNumCols == other.mNumCols);
         Matrix result(mNumRows, mNumCols);
@@ -78,7 +79,7 @@ public:
                 result(i, j) = mData[i][j] + other.mData[i][j];
         return result;
     }
-
+//Subtraction
     Matrix operator-(const Matrix& other) const {
         assert(mNumRows == other.mNumRows && mNumCols == other.mNumCols);
         Matrix result(mNumRows, mNumCols);
@@ -87,7 +88,7 @@ public:
                 result(i, j) = mData[i][j] - other.mData[i][j];
         return result;
     }
-
+//Multiplication of matrix
     Matrix operator*(const Matrix& other) const {
         assert(mNumCols == other.mNumRows);
         Matrix result(mNumRows, other.mNumCols);
@@ -97,7 +98,7 @@ public:
                     result(i, j) += mData[i][k] * other.mData[k][j];
         return result;
     }
-
+//Scalar operator
     Matrix operator*(double scalar) const {
         Matrix result(mNumRows, mNumCols);
         for (int i = 0; i < mNumRows; ++i)
@@ -113,7 +114,7 @@ public:
                 result(j, i) = mData[i][j];
         return result;
     }
-
+//determinant of matrix
     double determinant() const {
         assert(mNumRows == mNumCols);
         if (mNumRows == 1) return mData[0][0];
@@ -133,7 +134,7 @@ public:
         }
         return det;
     }
-
+// Gause-Jordan elimination to find the inverse
     Matrix inverse() const {
         assert(mNumRows == mNumCols);
         int n = mNumRows;
@@ -169,7 +170,7 @@ public:
 
         return inv;
     }
-
+//psuedo inverse 
     Matrix pseudoInverse() const {
         if (mNumRows >= mNumCols) {
             Matrix At = transpose();
