3407699a26c4f13d188d1fe9147bb53b2310d5df
Baocute123 <10423009@student.vgu.edu.vn>
2025-06-04 21:12:19 +0700

    Matrix.h

--- Matrix.h
+++ Matrix.h
@@ -13,22 +13,23 @@ private:
     void freeMemory();
 
 public:
+//Constructor and Destructor
     Matrix(int rows, int cols);
     Matrix(const Matrix& other);
     Matrix& operator=(const Matrix& other);
     ~Matrix();
-
+//Accessors
     int Rows() const;
     int Cols() const;
-
+//Overload bracket for element access
     double& operator()(int i, int j);
     const double& operator()(int i, int j) const;
-
+//Addition, Subtraction, Multiplication and Scalar
     Matrix operator+(const Matrix& other) const;
     Matrix operator-(const Matrix& other) const;
     Matrix operator*(const Matrix& other) const;
     Matrix operator*(double scalar) const;
-
+//Determinant, Inverse and psuedoInverse of Matrix
     Matrix transpose() const;
     double determinant() const;
     Matrix inverse() const;
