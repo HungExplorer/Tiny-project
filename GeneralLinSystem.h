4172d849ba03c955b9bc2dd778f47170c63a8b40
paolunn1336 <10423010@student.vgu.edu.vn>
2025-06-07 09:01:33 +0700

    GeneralLinSystem.h

--- GeneralLinSystem.h
+++ GeneralLinSystem.h
@@ -1,29 +1,22 @@
 #ifndef GENERALLINSYSTEM_H
 #define GENERALLINSYSTEM_H
-
 #include "Matrix.h"
 #include "Vector.h"
 #include <stdexcept>
-
 class GeneralLinSystem {
 private:
     int mRows;
     int mCols;
     Matrix* mpA;
     Vector* mpb;
-
 public:
     GeneralLinSystem(const Matrix& A, const Vector& b);
     ~GeneralLinSystem();
-
     int Rows() const;
     int Cols() const;
-
     Vector solvePseudoInverse() const;
     Vector solveTikhonov(double lambda) const;
-
     GeneralLinSystem(const GeneralLinSystem&) = delete;
     GeneralLinSystem& operator=(const GeneralLinSystem&) = delete;
 };
-
 #endif
