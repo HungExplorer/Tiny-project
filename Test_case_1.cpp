09291460a17a7452431a5ebc45fd834d0fd85798
Doan Quoc Hung <hungdoanvgu@gmail.com>
2025-06-07 12:48:25 +0700

    Update and rename Test_case_3.cpp to Test_case_1.cpp

--- Test_case_1.cpp
+++ Test_case_1.cpp
@@ -1,5 +1,3 @@
-// Ví dụ sử dụng trong main.cpp
-
 #include <iostream>
 #include "Matrix.h"
 #include "Vector.h"
@@ -9,14 +7,8 @@
 using namespace std;
 
 int main() {
-    // Tạo ma trận A (3x3) và vector b (3)
     Matrix A(3, 3);
     Vector b(3);
-
-    // Gán ví dụ:
-    // A = [ 4 1 0 ]
-    //     [ 1 3 1 ]
-    //     [ 0 1 2 ]
     A(1,1) = 4;  A(1,2) = 1;  A(1,3) = 0;
     A(2,1) = 1;  A(2,2) = 3;  A(2,3) = 1;
     A(3,1) = 0;  A(3,2) = 1;  A(3,3) = 2;
@@ -25,12 +17,12 @@ int main() {
     b(2) = 2;
     b(3) = 3;
 
-    cout << "--- Giải bằng Gaussian Elimination (LinearSystem) ---\n";
+    cout << "--- Solved by Gaussian Elimination (LinearSystem) ---\n";
     LinearSystem sys1(A, b);
     Vector x1 = sys1.solve();
     x1.display();
 
-    cout << "\n--- Giải bằng Conjugate Gradient (PosSymLinSystem) ---\n";
+    cout << "\n--- Solved by Conjugate Gradient (PosSymLinSystem) ---\n";
     PosSymLinSystem sys2(A, b);
     Vector x2 = sys2.solve();
     x2.display();
