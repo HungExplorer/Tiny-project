e612afe280e0466940208dcfea1264af51c38a2a
paolunn1336 <10423010@student.vgu.edu.vn>
2025-06-07 08:50:45 +0700

    Vector.h

--- Vector.h
+++ Vector.h
@@ -10,36 +10,24 @@ public:
     Vector(int size);
     Vector(const Vector& other);
     ~Vector();
-
     Vector& operator=(const Vector& other);
 
     int getSize() const;
 
-    // Truy cập phần tử, index từ 0 (operator[])
     double& operator[](int index);
-
-    // Truy cập phần tử, index từ 1 (operator())
     double& operator()(int index);
 
     Vector operator+(const Vector& other) const;
     Vector operator-(const Vector& other) const;
-
-    // Nếu muốn dot product trả về double:
     double operator*(const Vector& other) const;  
 
-    // Phép toán với scalar
     Vector operator+(double value) const;
     Vector operator-(double value) const;
     Vector operator*(double value) const;
+    Vector operator-() const;  
 
-    Vector operator-() const;  // unary minus
-
-    //Gán dữ liệu
     void manualAssign();
     void assign(double* val);
-
-    //Hiển thị
     void display() const; 
 };
-
 #endif
